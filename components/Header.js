ebe21324f0313291aabafc53faa00c162f75d1eb
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-10 01:53:06 +0500

    Non-Responsive Navbar Created

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,59 @@
 import React from "react";
-
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import PersonIcon from "@mui/icons-material/Person";
 const Header = () => {
   return (
     <div>
-      <div style={{backgroundImage: `url('/Images/ZIMO Pro.png')`,}} className="w-full object-cover h-[500px] bg-no-repeat bg-cover bg-center ">
-       <div className="text-white flex items-center w-full px-4 py-3">
-       <img
-              src="/Logo_s/ZIMO Menu Icon W.png"
-              alt="logo"
-              className="  w-7 mt-2"
-            />
-       <div className="ml-4 flex relative">
-       <img
+      <div
+        style={{ backgroundImage: `url('/Images/ZIMO Pro.png')` }}
+        className="w-full object-cover h-[500px] bg-no-repeat bg-cover bg-center">
+        <div className="text-white flex items-center w-full px-4 py-3">
+          <div className="left_logos flex">
+          <img
+            src="/Logo_s/ZIMO Menu Icon W.png"
+            alt="logo"
+            className="  w-7 mt-2"
+          />
+          <div className="ml-4 relative ">
+            <img
               src="/Logo_s/ZIMO W.png"
               alt="logo"
               className=" w-24 mt-2 hidden md:block"
             />
-            <p className="hidden md:block top-4 left-28 absolute  font-light text-sm  ">ABOUT</p>
-       </div>
-       <div className=" mx-auto">
-       <img className="w-32 mt-2" src="/Logo_s/ZIMO ZIG W.png" alt="Workflow logo"/>
-       </div>
-
-
-       </div>
+            <p className="hidden lg:block top-4 left-28 absolute  font-light text-md  ">
+              ABOUT
+            </p>
+          </div>
+          </div>
+          
+          <div className="lg:ml-[550px] lg:mr-[350px] mx-auto">
+            <img
+              className="w-36    mt-2"
+              src="/Logo_s/ZIMO ZIG W.png"
+              alt="Workflow logo"
+            />
+          </div>
+          
+          <div className="mt-2 hidden md:block p-2">
+            <p className="tracking-wider font-light text-xs text-white">
+              17:23 LONDON UNITED KINGDOM <br />
+              <span className="  text-yellow-400 ml-8">
+                SUNDAY, 12 FEBRUARY 2023
+              </span>
+            </p>
+          </div>
+          <img
+            src="/Images/England.com.png"
+            className="w-10 mt-2 hidden md:block"
+            alt="Eng logo"
+          />
+          <span className=" ml-2 text-white">
+            <ShoppingBagIcon />
+          </span>
+          <span className=" mx-2 text-white">
+            <PersonIcon />
+          </span>
+        </div>
       </div>
     </div>
   );
