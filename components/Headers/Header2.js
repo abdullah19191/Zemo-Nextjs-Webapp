f1bd2f72b5346bf430ccc2b757f70593fc9f2b65
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-10 23:14:18 +0500

    Navbar updated

--- components/Headers/Header2.js
+++ components/Headers/Header2.js
@@ -5,8 +5,8 @@ const Header2 = () => {
   return (
     <div className="px-8">
       <div className="px-3 text-white flex flex-col justify-between w-full md:items-start translate-y-[50%] relative top-[50px]">
-        <h3 className=" tracking-widest  text-2xl">DISCOVER</h3>
-        <h1 className="tracking-widest  text-3xl mt-2">A NEW WORLD</h1>
+        <h3 className=" tracking-widest text-2xl  md:text-3xl">DISCOVER</h3>
+        <h1 className="tracking-widest text-3xl  md:text-4xl mt-2">A NEW WORLD</h1>
         <p className="tracking-widest  text-xs font-normal text-gray-500 mt-4">
           FOR THOSE WHO WISH FOR MORE
         </p>
