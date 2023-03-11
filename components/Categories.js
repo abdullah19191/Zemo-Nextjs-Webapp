e01f102d761aefff7514cf1129e9a733a070490e
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-11 16:45:50 +0500

    Carosel completed

--- components/Categories.js
+++ components/Categories.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import {motion} from "framer-motion"
 
+const images = [
+      "/Images/CARS.png",
+      "/Images/JETS.jpg",
+      "/Images/WATCHES.png",
+      "/Images/PRIVATE ISLANDS.png",
+      "/Images/REAL ESTATE.png"
+      
+    ];
 const Categories = () => {
-  return (
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5,
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3,
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 234 },
+          items: 1,
+        },
+      };
+    return (
     <section className='w-full'>
           <div className='px-8 mt-28'>
               <div className=" flex justify-between items-center">
@@ -12,6 +42,35 @@ const Categories = () => {
                 <ArrowBackIosIcon style={{ fontSize: 30 ,color:"gray"}}/>
                 <ArrowForwardIosIcon style={{ fontSize: 30,color:"gray"}} />
                 </div>
+              </div>
+              <div className="  md:mt-8 mb-2 ">
+              <Carousel
+              centerMode={true}
+              swipeable={true}
+              draggable={false}
+              showDots={false}
+              responsive={responsive}
+              infinite={true}
+              autoPlay={true}
+              autoPlaySpeed={4000}
+              keyBoardControl={false}
+              //customTransition="all .5"
+              //transitionDuration={500}
+              containerClass="carousel-container"
+              useKeyboardArrows={false}
+              removeArrowOnDeviceType={["tablet", "mobile"]}
+              //deviceType={this.props.deviceType}
+              dotListClass="custom-dot-list-style"
+              itemClass="carousel-item-padding-40-px"
+              >
+                {
+                    images.map((url,index)=>(
+                <div className='min-w-[80%] min-h-[22rem] h-48 w-64   md:h-44 p-14 md:p-0 '>
+                    <img src={url} key={index} className='h-full w-full rounded-[2rem]' />
+                </div>
+                    ))
+                }
+            </Carousel>
               </div>
           </div>
     </section>
