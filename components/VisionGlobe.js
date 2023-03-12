99483e89a9b430688dadb2da8c2c4ddff5d09ad6
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-13 00:05:09 +0500

    Code formatted

--- components/VisionGlobe.js
+++ components/VisionGlobe.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const VisionGlobe = () => {
   return (
-    <div className='px-2 lg:px-6 '>
-        <div
+    <div className="px-2 lg:px-6 ">
+      <div
         style={{ backgroundImage: `url('/Images/World Map (dots).png')` }}
         className="flex flex-col justify-center items-center text-center object-cover md:h-[860px] h-[660px] bg-no-repeat bg-cover bg-center"
       >
         <h1 className="tracking-[.22rem] text-xl  md:text-2xl text-black font-normal">
-        GLOBAL VISION
-
-            </h1>
-            <p className="tracking-wide sm:tracking-wider text-xs font-[600] text-gray-900 mt-6">
-            WITH A REMOTE CULTURE, DIVERSITYIS NATURALLY IN OUR DNA</p>
-            <p className="tracking-wide sm:tracking-wider text-xs font-[600] text-gray-900 ">
-            BASED ACROS THE GLOBE, MAKING UP OVER 23 DIFFERENT NATIONALITIES
-            </p>
+          GLOBAL VISION
+        </h1>
+        <p className="tracking-wide sm:tracking-wider text-xs font-[600] text-gray-900 mt-6">
+          WITH A REMOTE CULTURE, DIVERSITYIS NATURALLY IN OUR DNA
+        </p>
+        <p className="tracking-wide sm:tracking-wider text-xs font-[600] text-gray-900 ">
+          BASED ACROS THE GLOBE, MAKING UP OVER 23 DIFFERENT NATIONALITIES
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VisionGlobe
+export default VisionGlobe;
